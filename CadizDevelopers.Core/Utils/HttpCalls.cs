202c302932a1ba074751afb12c3b9d3e5d3ca7f4
jmmortega <jmmontero.ortega@gmail.com>
2013-07-25 01:40:53 +0200

    Init commit

---
+++ CadizDevelopers.Core/Utils/HttpCalls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CadizDevelopers.Core.Utils
+{
+    public class HttpCalls
+    {
+        public void Post(string url, string contentType , Action<Stream> actionResponse)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.ContentType = contentType;
+            request.Method = "POST";
+
+            request.BeginGetResponse((resultResponse) =>
+            {
+                var beginResponse = (HttpWebRequest)resultResponse.AsyncState;
+
+                var response = (HttpWebResponse)beginResponse.EndGetResponse(resultResponse);
+
+                var streamResponse = response.GetResponseStream();
+
+                actionResponse.Invoke(streamResponse);
+            }, request);
+        }
+
+    }
+}
